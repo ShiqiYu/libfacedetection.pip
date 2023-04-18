4e31622efb8b0365ea4d1fb522c4eba6a0276fa4
Wwupup <wuweimyself@qq.com>
2023-04-18 10:05:29 +0800

    complie by pre_build.py

--- setup.py
+++ setup.py
@@ -10,44 +10,42 @@ import platform
 import sys
 import shutil
 
-
 __version__ = "0.0.1"
 
-dbr_lib_dir = ''
-dbr_include = ''
-dbr_lib_name = 'libfacedetection'
+lib_name = 'facedetection'
+lib_dir = ''
 
 # linux
 if sys.platform == "linux" or sys.platform == "linux2":
     if platform.uname()[4] == 'AMD64' or platform.uname()[4] == 'x86_64':
-            dbr_lib_dir = 'lib/linux'
+            lib_dir = 'lib/linux/x64/avx2'
     elif platform.uname()[4] == 'aarch64':
-            dbr_lib_dir = 'lib/aarch64'
+            lib_dir = 'lib/linux/aarch64/avx2'
     else:
-            dbr_lib_dir = 'lib/arm32'
+            lib_dir = 'lib/linux/arm/neon'
     ext_args = dict(        
         include_dirs=['include'],
-        libraries = [dbr_lib_name],
-        library_dirs = [dbr_lib_dir],
-        extra_compile_args = ['-std=c++11'],
+        libraries = [lib_name],
+        library_dirs = [lib_dir],
         extra_link_args = ["-Wl,-rpath=$ORIGIN"],
         define_macros = [('VERSION_INFO', __version__)],
         language='c++',
         cxx_std=11,
     )
+    
 # windows
-elif sys.platform == "win32":
-    dbr_lib_name = 'DBRx64'
-    dbr_lib_dir = 'lib/win'
+elif sys.platform == "win":
+    lib_dir = 'lib/win/x64/avx2'
     ext_args = dict(
         include_dirs=['include'], 
-        libraries=[dbr_lib_name],
-        library_dirs = [dbr_lib_dir],
+        libraries=[lib_name],
+        library_dirs = [lib_dir],
+        extra_link_args = ["-Wl,-rpath=$ORIGIN"],
         define_macros = [('VERSION_INFO', __version__)],
         language='c++',
         cxx_std=11,
     )
-        
+
 yudet_modules = [
     Pybind11Extension(
         "yudet",
@@ -60,38 +58,69 @@ def copylibs(src, dst):
     if os.path.isdir(src):
         filelist = os.listdir(src)
         for file in filelist:
-                libpath = os.path.join(src, file)
-                shutil.copy2(libpath, dst)
+            libpath = os.path.join(src, file)
+            shutil.copy2(libpath, dst)
     else:
         shutil.copy2(src, dst)
 
 class CustomBuildExt(build_ext):
     def run(self):
         build_ext.run(self)
-        dst =  os.path.join(self.build_lib, "barcodeQrSDK")
-        copylibs(dbr_lib_dir, dst)
+        dst = os.path.join(self.build_lib, "yudet")
+        copylibs(lib_dir, dst)
         filelist = os.listdir(self.build_lib)
         for file in filelist:
             filePath = os.path.join(self.build_lib, file)
-            if not os.path.isdir(file):
+            if not os.path.isdir(filePath):
                 copylibs(filePath, dst)
                 # delete file for wheel package
                 os.remove(filePath)
 
-class CustomBuildExtDev(build_ext):
-    def run(self):
-        build_ext.run(self)
-        dev_folder = os.path.join(os.path.dirname(__file__), 'barcodeQrSDK')
-        copylibs(dbr_lib_dir, dev_folder)
-        filelist = os.listdir(self.build_lib)
-        for file in filelist:
-            filePath = os.path.join(self.build_lib, file)
-            if not os.path.isdir(file):
-                copylibs(filePath, dev_folder)
+# class CustomBuildExtDev(build_ext):
+#     def run(self):
+#         build_ext.run(self)
+#         dev_folder = os.path.join(os.path.dirname(__file__), 'barcodeQrSDK')
+#         copylibs(lib_dir, dev_folder)
+#         filelist = os.listdir(self.build_lib)
+#         for file in filelist:
+#             filePath = os.path.join(self.build_lib, file)
+#             if not os.path.isdir(file):
+#                 copylibs(filePath, dev_folder)
 
-class CustomInstall(install):
-    def run(self):
-        install.run(self)
+# class CustomInstall(install):
+#     def run(self):
+#         install.run(self)
 
-setup(ext_modules=yudet_modules, 
-      cmdclass={'build_ext': CustomBuildExt, 'install': CustomInstall, 'develop': CustomBuildExtDev})
+setup(
+    name="yudet",
+    keywords = ["face detection"],
+    version = "0.0.1",
+    author="Wwupup",
+    author_email="12032501@mail.sustech.edu.cn",
+    url="https://github.com/Wwupup/libfacedetection.pypi",
+    description="A face detection library based on libfacedetection",
+    license="BSD",
+    packages=["yudet"],
+    package_dir={"yudet": "src/yudet"},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: C++",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Classifier: Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Intended Audience :: Developers",
+    ],
+    ext_modules=yudet_modules, 
+    cmdclass={'build_ext': CustomBuildExt}
+)
---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+import build.yudet as m
+
+
+def test_main():
+    print(dir(m))
+    # assert m.__version__ == "0.0.1"
+    # assert m.detect == 3
+    # assert m.subtract(1, 2) == -1
